75d80c32261c28785e35023c579e5d92c4e23473
abel <abel@gmail.com>
2021-03-14 17:40:48 +0800

    x消息推送

---
+++ src/web-push/server/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  VAPIDKeys: {
+    publicKey: 'BJzBzcCmsCtBS4qfzXuoTEaG8i0jByosS8wDaHqDaCodMDurm0gdNbbNkjEmfHuGOWDabbDNbxhBnT0TrqRgOlM',
+    privateKey: 'aG8mwolsZ4VfVepVLNKmgffe4pdkt8ZC7gYsKs8Nczs'
+  },
+  GCMAPIkey: 'FCM Public Key'
+}
